e1b6f1ef3c080b191dca058bbb7968bf55390e5d
Jonas Frost <j@duplo.org>
2015-06-02 15:23:21 -0700

    Bombsquad Caching

--- Bmbsqd.Caching/ICacheExpire.cs
+++ Bmbsqd.Caching/ICacheExpire.cs
@@ -1,36 +1,7 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-﻿namespace Bmbsqd.Caching
+namespace Bmbsqd.Caching
 {
 	internal interface ICacheExpire
 	{
 		void InvalidateExpiredItems();
 	}
 }
-||||||| (empty tree)
-=======
-﻿namespace Bmbsqd.Caching
-{
-	internal interface ICacheExpire
-	{
-		void InvalidateExpiredItems();
-	}
-}
->>>>>>> 46949d2... initial
-||||||| parent of cebcaf3... updated
-﻿namespace Bmbsqd.Caching
-{
-	internal interface ICacheExpire
-	{
-		void InvalidateExpiredItems();
-	}
-}
-=======
-﻿namespace Bmbsqd.Caching
-{
-	internal interface ICacheExpire
-	{
-		void InvalidateExpiredItems();
-	}
-}
->>>>>>> cebcaf3... updated
--- Bmbsqd.Caching/ICacheInvalidate.cs
+++ Bmbsqd.Caching/ICacheInvalidate.cs
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 using System;
 
 namespace Bmbsqd.Caching
@@ -19,62 +17,3 @@ namespace Bmbsqd.Caching
 		bool TryUpdate( TKey key, TValue value );
 	}
 }
-||||||| (empty tree)
-=======
-namespace Bmbsqd.Caching
-{
-	public interface ICacheInvalidate
-	{
-		void InvalidateAll();
-	}
-
-	public interface ICacheInvalidate<in TKey> : ICacheInvalidate
-	{
-		bool Invalidate( TKey key );
-	}
-
-	public interface ICacheUpdate<in TKey, in TValue>
-	{
-		bool TryUpdate( TKey key, TValue value );
-	}
-}
->>>>>>> 46949d2... initial
-||||||| parent of cebcaf3... updated
-namespace Bmbsqd.Caching
-{
-	public interface ICacheInvalidate
-	{
-		void InvalidateAll();
-	}
-
-	public interface ICacheInvalidate<in TKey> : ICacheInvalidate
-	{
-		bool Invalidate( TKey key );
-	}
-
-	public interface ICacheUpdate<in TKey, in TValue>
-	{
-		bool TryUpdate( TKey key, TValue value );
-	}
-}
-=======
-using System;
-
-namespace Bmbsqd.Caching
-{
-	public interface ICacheInvalidate : IDisposable
-	{
-		void InvalidateAll();
-	}
-
-	public interface ICacheInvalidate<in TKey> : ICacheInvalidate
-	{
-		bool Invalidate( TKey key );
-	}
-
-	public interface ICacheUpdate<in TKey, in TValue>
-	{
-		bool TryUpdate( TKey key, TValue value );
-	}
-}
->>>>>>> cebcaf3... updated

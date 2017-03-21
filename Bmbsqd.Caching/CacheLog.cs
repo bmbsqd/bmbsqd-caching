e491e23fb04cbf4ddb520fc4f313e830e3c93b1e
Jonas Frost <j@duplo.org>
2017-03-21 14:36:44 -0700

    upaded to 1.1

--- Bmbsqd.Caching/CacheLog.cs
+++ Bmbsqd.Caching/CacheLog.cs
@@ -1,9 +1,7 @@
 using System;
-using JetBrains.Annotations;
 
 namespace Bmbsqd.Caching {
 	public static class CacheLog {
-		[StringFormatMethod( "format" )]
 		public static void Error( Exception exception, string format, params object[] args ) { }
 		public static void Error( Exception exception, string message ) { }
 	}

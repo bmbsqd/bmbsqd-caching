4dbe9a4a70ec6643d4dc8c900ef5e519b10320b9
Jonas Frost <j@duplo.org>
2022-06-09 22:29:41 +0700

    added test for ejecting faulted async entries faster

--- Bmbsqd.Caching.Tests/CacheTest.cs
+++ Bmbsqd.Caching.Tests/CacheTest.cs
@@ -6,88 +6,84 @@ using Xunit;
 
 namespace Bmbsqd.Caching.Tests;
 
-public class CacheTest {
+public class CacheTest
+{
 	[Fact]
-	public void SyncCache()
-	{
-		var c = new Cache<string, string>( TimeSpan.FromSeconds( 1 ) );
+	public void SyncCache() {
+		var c = new Cache<string, string>(TimeSpan.FromSeconds(1));
 
-		Assert.Equal( "world", c.GetOrAdd( "hello", k => "world" ) );
-		Assert.Equal( "world", c.GetOrAdd( "hello", k => "no-no-no" ) ); // should still be "world"
+		Assert.Equal("world", c.GetOrAdd("hello", k => "world"));
+		Assert.Equal("world", c.GetOrAdd("hello", k => "no-no-no")); // should still be "world"
 
-		Thread.Sleep( TimeSpan.FromSeconds( 1.1 ) );
+		Thread.Sleep(TimeSpan.FromSeconds(1.1));
 		c.InvalidateExpiredItems();
 
-		Assert.Equal( "universe", c.GetOrAdd( "hello", k => "universe" ) );
+		Assert.Equal("universe", c.GetOrAdd("hello", k => "universe"));
 	}
 
 	[Fact]
-	public async Task AsyncCache()
-	{
-		var c = new AsyncCache<string, string>( TimeSpan.FromSeconds( 1 ) );
+	public async Task AsyncCache() {
+		var c = new AsyncCache<string, string>(TimeSpan.FromSeconds(1));
 
-		Assert.Equal( "world", await c.GetOrAddAsync( "hello", k => Task.FromResult( "world" ) ) );
-		Assert.Equal( "world", await c.GetOrAddAsync( "hello", k => Task.FromResult( "no-no-no" ) ) ); // should still be "world"
+		Assert.Equal("world", await c.GetOrAddAsync("hello", k => Task.FromResult("world")));
+		Assert.Equal("world", await c.GetOrAddAsync("hello", k => Task.FromResult("no-no-no"))); // should still be "world"
 
-		await Task.Delay( TimeSpan.FromSeconds( 1.5 ) );
+		await Task.Delay(TimeSpan.FromSeconds(1.5));
 		c.InvalidateExpiredItems();
 
-		Assert.Equal( "universe", await c.GetOrAddAsync( "hello", k => Task.FromResult( "universe" ) ) );
+		Assert.Equal("universe", await c.GetOrAddAsync("hello", k => Task.FromResult("universe")));
 	}
 
 	[Fact]
-	public async Task ReturnExpiredItems()
-	{
-		var c = new AsyncCache<string, string>( TimeSpan.FromSeconds( 0.1 ), false, true );
-		Assert.Equal( "world", await c.GetOrAddAsync( "hello", k => Task.FromResult( "world" ) ) );
-		Assert.Equal( "world", await c.GetOrAddAsync( "hello", k => Task.FromResult( "no-no-no" ) ) ); // should still be "world"
-		await Task.Delay( TimeSpan.FromSeconds( 0.5 ) );
-		Assert.Equal( "world", await c.GetOrAddAsync( "hello", async k => {
+	public async Task ReturnExpiredItems() {
+		var c = new AsyncCache<string, string>(TimeSpan.FromSeconds(0.1), false, true);
+		Assert.Equal("world", await c.GetOrAddAsync("hello", k => Task.FromResult("world")));
+		Assert.Equal("world", await c.GetOrAddAsync("hello", k => Task.FromResult("no-no-no"))); // should still be "world"
+		await Task.Delay(TimeSpan.FromSeconds(0.5));
+		Assert.Equal("world", await c.GetOrAddAsync("hello", async k => {
 			// A completed task is always returned
 			// So to prevent that, we have to delay here :-)
-			await Task.Delay( 25 );
+			await Task.Delay(25);
 			return "universe";
-		} ) );
+		}));
 	}
 
 	[Fact]
-	public async Task DontReturnExpiredItems()
-	{
-		var c = new AsyncCache<string, string>( TimeSpan.FromSeconds( 0.1 ), returnExpiredItems: false );
-		Assert.Equal( "world", await c.GetOrAddAsync( "hello", k => Task.FromResult( "world" ) ) );
-		Assert.Equal( "world", await c.GetOrAddAsync( "hello", k => Task.FromResult( "no-no-no" ) ) ); // should still be "world"
-		await Task.Delay( TimeSpan.FromSeconds( 0.5 ) );
-		Assert.Equal( "universe", await c.GetOrAddAsync( "hello", k => Task.FromResult( "universe" ) ) );
+	public async Task DontReturnExpiredItems() {
+		var c = new AsyncCache<string, string>(TimeSpan.FromSeconds(0.1), returnExpiredItems: false);
+		Assert.Equal("world", await c.GetOrAddAsync("hello", k => Task.FromResult("world")));
+		Assert.Equal("world", await c.GetOrAddAsync("hello", k => Task.FromResult("no-no-no"))); // should still be "world"
+		await Task.Delay(TimeSpan.FromSeconds(0.5));
+		Assert.Equal("universe", await c.GetOrAddAsync("hello", k => Task.FromResult("universe")));
 	}
 
 
 	[Fact]
-	public async Task AsyncCache2()
-	{
-		var c = new AsyncCache<string, string>( TimeSpan.FromMilliseconds( 100 ), false, true );
+	public async Task AsyncCache2() {
+		var c = new AsyncCache<string, string>(TimeSpan.FromMilliseconds(100), false, true);
 
-		Assert.Equal( "world", await c.GetOrAddAsync( "hello", k => Task.FromResult( "world" ) ) );
+		Assert.Equal("world", await c.GetOrAddAsync("hello", k => Task.FromResult("world")));
 
-		await Task.Delay( TimeSpan.FromMilliseconds( 200 ) );
-		Assert.Equal( "world", await c.GetOrAddAsync( "hello", async k => {
+		await Task.Delay(TimeSpan.FromMilliseconds(200));
+		Assert.Equal("world", await c.GetOrAddAsync("hello", async k => {
 			// A completed task is always returned
 			// So to prevent that, we have to delay here :-)
-			await Task.Delay( 25 );
+			await Task.Delay(25);
 			return "universe";
-		} ) );
+		}));
 
-		await Task.Delay( TimeSpan.FromMilliseconds( 200 ) );
-		Assert.Equal( "universe", await c.GetOrAddAsync( "hello", async k => {
+		await Task.Delay(TimeSpan.FromMilliseconds(200));
+		Assert.Equal("universe", await c.GetOrAddAsync("hello", async k => {
 			// A completed task is always returned
 			// So to prevent that, we have to delay here :-)
-			await Task.Delay( 25 );
+			await Task.Delay(25);
 			return "universe";
-		} ) );
+		}));
 	}
 
-	public class D : IDisposable {
-		public void Dispose()
-		{
+	public class D : IDisposable
+	{
+		public void Dispose() {
 			IsDisposed = true;
 		}
 
@@ -95,37 +91,48 @@ public class CacheTest {
 	}
 
 	[Fact]
-	public async Task IsDisposingDisposables()
-	{
-		var c = new AsyncCache<string, D>( TimeSpan.FromMinutes( 1 ), true );
+	public async Task IsDisposingDisposables() {
+		var c = new AsyncCache<string, D>(TimeSpan.FromMinutes(1), true);
 
-		var d = await c.GetOrAddAsync( "abc", k => Task.FromResult( new D() ) );
-		Assert.False( d.IsDisposed );
+		var d = await c.GetOrAddAsync("abc", k => Task.FromResult(new D()));
+		Assert.False(d.IsDisposed);
 		c.InvalidateAll();
-		Assert.True( d.IsDisposed );
+		Assert.True(d.IsDisposed);
 	}
 
 
 	[Fact]
-	public async Task MultiGetOrAdd()
-	{
-		var c = new AsyncCache<string, string>( TimeSpan.FromMinutes( 1 ), true );
+	public async Task MultiGetOrAdd() {
+		var c = new AsyncCache<string, string>(TimeSpan.FromMinutes(1), true);
 
-		var r = await c.GetOrAddAsync( "a", k => Task.FromResult( "foo" ) );
-		Assert.Equal( "foo", r );
+		var r = await c.GetOrAddAsync("a", k => Task.FromResult("foo"));
+		Assert.Equal("foo", r);
 
 
-		var m = await c.GetOrAddAsync( new[] { "a", "b", "c" }, k => Task.FromResult<IDictionary<string, string>>( new Dictionary<string, string> {
-			{"a", null },
-			{"b", "bar" },
-			{"c", "baz" },
-		} ) );
+		var m = await c.GetOrAddAsync(new[] { "a", "b", "c" }, k => Task.FromResult<IDictionary<string, string>>(new Dictionary<string, string> {
+			{ "a", null },
+			{ "b", "bar" },
+			{ "c", "baz" },
+		}));
 
-		Assert.Equal( 3, m.Count );
-		Assert.Contains( "a", m.Keys );
-		Assert.Contains( "b", m.Keys );
-		Assert.Contains( "c", m.Keys );
+		Assert.Equal(3, m.Count);
+		Assert.Contains("a", m.Keys);
+		Assert.Contains("b", m.Keys);
+		Assert.Contains("c", m.Keys);
 
-		Assert.Contains( "foo", m["a"] );
+		Assert.Contains("foo", m["a"]);
+	}
+
+	[Fact]
+	public async Task FaultedTasksShouldBeInvalidatedFaster() {
+		var c = new AsyncCache<string, string>(TimeSpan.FromMinutes(1), true);
+
+		var t = c.GetOrAddAsync("a", _ => Task.FromException<string>(new Exception("Hello World")));
+		
+		Assert.True(t.IsFaulted);
+		Assert.Equal(1, c.Count);
+		
+		c.InvalidateExpiredItems();
+		Assert.Equal(0, c.Count);
 	}
 }

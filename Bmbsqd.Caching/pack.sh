cff668374e3d49c36f4e38a2195ed2606218141b
Jonas Frost <j@duplo.org>
2024-06-13 20:52:13 +0700

    just updated packing and pushing scripts

--- Bmbsqd.Caching/pack.sh
+++ Bmbsqd.Caching/pack.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 CONFIG=Release
+rm bin/$CONFIG/Bmbsqd.Caching.*.nupkg
 dotnet build --nologo true --configuration $CONFIG
-dotnet pack --nologo true --include-symbols true --include-source true --configuration $CONFIG
+dotnet pack --nologo true --include-source true --configuration $CONFIG
+dotnet nuget push bin/$CONFIG/Bmbsqd.Caching.*.nupkg --source https://api.nuget.org/v3/index.json --api-key $NUGET_API_KEY --skip-duplicate
